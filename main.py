0a6cde371310ffae5c09bba1eace9c3fd451b094
robert_bao <ibaochen@gmail.com>
2022-02-24 19:33:57 -0500

    Improve code style

--- main.py
+++ main.py
@@ -4,23 +4,18 @@
 # File: main.py
 # The signal analysis code for pedometer analysis
 
-import csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import pandas as pd
 
 WINDOW_SIZE = 100
 
-# best way to get data from the phone sensor:
-# - MOVE THE ARM == BAD
-#   - requires move hand FORWARD and BACKWARD
-# - Better: put in the pants
-# best case:
-# - 1 valley + 1 peak
-# - constant amplitude
-
-# Find local maxima is okay
-# could have errors though
+# OH Notes:
+# The best way to get data from the phone sensor: put in the pants
+# Hold the phone in hands is BAD
+# - requires move hand FORWARD and BACKWARD
+# - could be difficult
+# Find local maxima is okay -- could have errors though
 
 
 # round a number to 2 decimal points
@@ -37,53 +32,53 @@ def get_rolling_avg(input_series, window_size):
     return moving_averages[window_size - 1:]
 
 
-filename = "data/walk-10-step-2022-2-24-v2.csv"
+# thresholding function
+def threshold_fn(x, threshold=3, step=2):
+    res = pd.Series([0] * len(x), index=x.index)
+    t = x.min() + threshold
+    res.loc[x.gt(t)] = step
+    return res
+
+
+filename = "data/walk-10-step-2022-2-24-v1.csv"
 df = pd.read_csv(filename)
 time = df["Time (s)"]
 acc_x = df["Acceleration y (m/s^2)"]
-print(type(acc_x))
 
 avg_time = get_rolling_avg(time, WINDOW_SIZE)
 avg_acc_x = get_rolling_avg(acc_x, WINDOW_SIZE)
 
 # if the data trend is increasing
 series_avg_acc_x = pd.Series(avg_acc_x)
-incr = series_avg_acc_x.diff().ge(0)
+increasing_elements = series_avg_acc_x.diff().ge(0)
 
 # shifted trend (local minima)
-shifted = incr.ne(incr.shift())
+shifted = increasing_elements.ne(increasing_elements.shift())
 
-# local max
-local_max = shifted & (~incr)
+# find the local max
+local_max = shifted & (~increasing_elements)
 
-
-# thresholding function
-def thresh(x, threshold=3, step=2):
-    ret = pd.Series([0] * len(x), index=x.index)
-    t = x.min() + threshold
-    ret.loc[x.gt(t)] = step
-    return ret
-
-
-signal = series_avg_acc_x.groupby(local_max.cumsum()).apply(thresh)
-signal += series_avg_acc_x.min()
-# print(signal[200:])
-
-plt.plot(avg_time, avg_acc_x)
-plt.xlabel('Time (s)')
-plt.ylabel('Acceleration (m/s^2)')
+# generate the step data by grouping sthe acceleration by the threshold
+step = series_avg_acc_x.groupby(local_max.cumsum()).apply(threshold_fn)
+step += series_avg_acc_x.min()
 
 # difference from the previous row
-signal_change = signal - signal.shift(1)
+step_change = step - step.shift(1)
 
-print(signal_change[:200])
-value_count = signal_change.value_counts()
-print(value_count)
-print(type(value_count))
+value_count = step_change.value_counts()
+positive_count = value_count[value_count.index > 0].iloc[0]
 
-plt.plot(avg_time, signal, drawstyle='steps')
+plt.plot(avg_time, avg_acc_x)
+plt.plot(avg_time, step, drawstyle='steps')
 
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
 plt.gca().yaxis.set_major_locator(MaxNLocator(5))
 plt.gca().xaxis.set_major_locator(MaxNLocator(10))
 
 plt.show()
+
+print("=========================")
+print("The Pedometer Algoirthm result")
+print("Total step:", positive_count)
+print("=========================")
